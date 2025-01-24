e5141c1d3e7342d1877edc47483078abdb1e2fab
hamza <hamzapervaiz8@gmail.com>
2025-01-24 12:23:52 +0500

    [HIDE] tax menue in sale order line

---
+++ sale_order_tax_hide/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Hide Taxes on Sale Order Line",
+    "version": "18.0",
+    "category": "Sales",
+    "summary": "Hides the tax_id field from Sale Order Line in the Sale Order form.",
+    "description": """
+        This module customizes the Sale Order form by hiding the tax_id field 
+        in the Sale Order Line to simplify the user interface for specific business needs.
+    """,
+    "author": "Hamza Pervaiz",
+    "license": "LGPL-3",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_order_form_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
