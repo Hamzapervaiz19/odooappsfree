bb10d363416323e04fa4d89255b7957cac478d1b
hamza <hamzapervaiz8@gmail.com>
2025-01-24 17:13:45 +0500

    change banner image extension

--- sale_order_tax_hide/__manifest__.py
+++ sale_order_tax_hide/__manifest__.py
@@ -13,7 +13,7 @@
     "data": [
         "views/sale_order_form_view.xml",
     ],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     "installable": True,
     "application": False,
     "auto_install": False,
